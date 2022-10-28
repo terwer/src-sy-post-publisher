c355d3e4095c8be2b21eee3e12fc54e5b881b271
terwer <youweics@163.com>
2022-10-28 11:53:14 +0800

    feat: #140 明确版本边界-Nuxt3配合pnpm项目初始化

---
+++ components/AppAlert.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>
+    <slot />
+  </span>
+</template>
+
+<script lang="ts">
+export default {
+  name: "AppAlert"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-imported component
+    </AppAlert>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
